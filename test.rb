0285b6603a689829948c3c083faf01339aabb57e
bryan lesht <bryan.lesht@colorado.edu>
2020-02-20 21:47:46 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -19,20 +19,3 @@ results = {
     }
   ]
 }
-
-puts results[:winners][-1][:margin]
-
-bands = {
-  joy_division: %w[ian bernard peter stephen],
-  the_smiths: %w[johnny andy morrissey mike],
-  the_cramps: %w[lux ivy nick],
-  blondie: %w[debbie chris clem jimmy nigel],
-  talking_heads: %w[david tina chris jerry]
-}
-
-bands.reduce({}) do |memo, (key, value)|
-  p memo # First block parameter
-  p key # Second block parameter
-  p value # Second block parameter
-  memo # Return value for the block, becomes the memo in the next go-round
-end
